7d5025212171dabb8bd6e9a477d13f42a5ea436c
evakoshy1-bit <evakoshy1@gmail.com>
2026-03-17 23:32:45 -0400

    fix

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -1,20 +1,5 @@
 """
-This is to simulate a change made on a robot: robot_speed = 3 # m/s 
-lab_1a.py
-
-The first lab in the BWSI CSS course. To complete this lab, fill out the variable on line 10
-with your name. Then, save the code, add it to the staging area, and commit it to the Git tree.
-"""
-
-def main():
-    print("Hello World!")
-
-    name = "Eva" # TODO: Insert your name between the double quotes
-
-    print(f"{name}, Welcome to the CSS course!")
-    print("Hi, my name is Eva. I am a junior in highschool, and I enjoy learning about programming!")  
-if __name__ == "__main__":
-    main()
+This is to simulate a change made on a robot: robot_speed = 5 # m/s 
 lab_1a.py
 
 The first lab in the BWSI CSS course. To complete this lab, fill out the variable on line 10
